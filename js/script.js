b17962c6efdf73335913fc52c4492bf647e66609
unknown <Usager@SGISR-DSITDG.FECECAM.LOCAL>
2026-03-30 00:34:49 +0100

    Switch modele flash-lite

--- js/script.js
+++ js/script.js
@@ -265,7 +265,7 @@
 
     try {
       const res = await fetch(
-  `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=AIzaSyCW-XXS6zvkYmgPHkP7exC71YphAALednI`,
+  `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash-lite:generateContent?key=AIzaSyCW-XXS6zvkYmgPHkP7exC71YphAALednI`,
   {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
